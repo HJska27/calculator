ff310654f8f35603f9136a6b64862f37a3404848
HJska27 <kallvey.thong23@student.cart.org>
2024-01-25 09:49:40 -0800

    gfd

--- function.js
+++ function.js
@@ -1,3 +1,37 @@
+function myFunction() {
+    const op = prompt("Enter Operator", "+,-,*,/");
 
-let ten = prompt ('Choose numbers')
-console.log(ten * ten);
+
+    const number1 = parseFloat(prompt("enter first number"));
+    const number2 = parseFloat(prompt("enter second number"));
+
+
+    switch (op) {
+        case '+':
+            result = number1 + number2;
+            alert(`${number1} + ${number2} = ${result}`);
+            break;
+
+        case '-':
+            result = number1 - number2;
+            alert(`${number1} - ${number2} = ${result}`);
+            break;
+
+        case '*':
+            result = number1 * number2;
+            alert(`${number1} * ${number2} = ${result}`);
+            break;
+
+        case '/':
+            result = number1 / number2;
+            alert(`${number1} / ${number2} = ${result}`);
+            break;
+
+        default:
+            alert('Invalid operator');
+            break;
+    }
+
+
+
+}
